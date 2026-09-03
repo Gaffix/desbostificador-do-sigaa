bd59d1b6e1b370e2220a4ed6b768a2156e496030
jappejappe <gabrieljappemoura95071@gmail.com>
2026-09-03 01:06:13 -0300

    feature: remoções da DIV Pefil Docente

--- content/reorganizar.js
+++ content/reorganizar.js
@@ -1,1 +1,8 @@
 // Arquivo responsável por manipular o DOM e reorganizar os elementos da página
+
+window.addEventListener('load', function() {
+    // Remove os botões específicos e deixa o Pergamum e o Meu Pergamum intactos
+    document.getElementById('form_links:forumCursos')?.remove();
+    document.getElementById('form_links:buscarComunidadeVirtual')?.remove();
+    document.querySelector('a[title*="CAPES"]')?.remove();
+});
